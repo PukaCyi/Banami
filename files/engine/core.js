4a5d5ff5297149be72059181f37af1cb1bcb50a7
Banami <149914478+PukaCyi@users.noreply.github.com>
2024-06-23 16:55:17 -0500

    Syncing | current version: 2024.23.06.vespatir

--- files/engine/core.js
+++ files/engine/core.js
@@ -1,10 +1,12 @@
-error = new Audio("https://banami.nekoweb.org/files/sounds/nope.mp3");
-error.volume = 0.5;
+let error = new Audio("https://banami.nekoweb.org/files/sounds/nope.mp3");
+let enter = new Audio("https://banami.nekoweb.org/files/sounds/heal.mp3");
+let hueh = new Audio("https://banami.nekoweb.org/files/sounds/bg.mp3");
+hueh.loop = true;
 
-enter = new Audio("https://banami.nekoweb.org/files/sounds/heal.mp3");
+/* def volumes after audios loaded */
 enter.volume = 0.5;
-
-hueh = new Audio("https://banami.nekoweb.org/files/sounds/bg.mp3");
+error.volume = 0.5;
+hueh.volume = 0.95;
 
 class Trace {
   constructor() {
@@ -16,34 +18,55 @@ class Trace {
     const pat = `color: ${this.color[ty]}; font-weight: bold; font-family: Consolas, monospace; font-size: 13px;`;
 
     if (nono.length > 0 || !(ty in this.tracertype)) {
-      this.trace("An error occurred while attempting to execute a trace command. Was the trace command written correctly?", "warn");
+      console.error(`%c[ ${this.tracertype.warn} ] %cAn error occurred while attempting to execute a trace command. Was the trace command written correctly?`, pat, '');
     } else if (ty === "warn") {
-      console.error(`%c[ ${this.tracertype[ty]} ] %c${txt}`, pat, '');
+      console.error(`%c[ ${this.tracertype.warn} ] %c${txt}`, pat, '');
     } else if (ty === "note") {
-      console.warn(`%c[ ${this.tracertype[ty]} ] %c${txt}`, pat, '');      
+      console.warn(`%c[ ${this.tracertype.note} ] %c${txt}`, pat, '');      
     }
   }
 }
 
 const engineLog = new Trace();
 const trace = engineLog.trace.bind(engineLog);
-trace("Trace logger is active.", "warn");
+/** For some reason, periods at the end of "org" still makes the site load - though that's broken so we'll redirect
+ * user to actual site lolz **/
+ 
+/** take notes: 
+ * window.location.hostname = gives cur url
+ * window.location.protocol = cur protocol (https more than likely) - used this to prevent accidental dos
+ * currentPos = where the user was at (ex: /dev?urltext_598345928) | merge of hash, pathname, and search
+**/
+
+if (window.location.hostname !== "banami.nekoweb.org" || window.location.protocol !== "https:") {// do not remove location protocol, that will dos the site.
+    currentPos = window.location.pathname + window.location.search + window.location.hash;
+    
+    trace("Invalid path, correcting\n\nUser was at:" + window.location.hostname + window.location.pathname + window.location.search + window.location.hash + "\nprotocol used: "+ window.location.protocol, "warn");
+    window.location.href = "https://banami.nekoweb.org" + currentPos;
+}
+
+trace("Logger is running :).", "note");
+
 
 window.addEventListener('error', function(event) {
   const cause = event.message;
   const orgin = event.filename;
   const linum = event.lineno;
   const linno = event.colno;
-  const error = event.error || new Error(cause);
+  const errorObj = event.error || new Error(cause);
 
-  trace(`An error occurred during runtime\n${orgin}:${linum}: ${error}`,"warn");
+  trace(`Exception occurred during runtime: \n${orgin}:${linum}: ${errorObj.stack}`, "warn");
   event.preventDefault(); 
   return true;
 });
 
 function hikki() {
   const playAudio = () => {
-    hueh.play().then(() => { });
+            try {
+            hueh.play();
+        } catch (error) {
+            trace(`Can't play :(: ${error.message}`, "warn");
+        }
   };
 
   hueh.addEventListener("ended", () => {
@@ -62,14 +85,18 @@ function nekoPage(pageName) {
 
   if (currentPage === pageName) {
     trace("Already on the same page", "note");
-    error.play();
-    trace("Error play!", "note");
+    try {
+        trace("Error play!", "note");
+        error.play();
+    } catch (error) {
+        trace(`Can't play :(: ${error.message}`, "warn");
+    }
     return;
   }
 
   const titleMain = document.getElementById("neko-title");
   const textMain = document.getElementById("neko-text");
-  const body = document.body;
+  const html = document.documentElement; 
   const backButton = document.querySelector(".neko-back-button");
   const banamiPfp = document.getElementById("bana-img-pfp");
 
@@ -79,8 +106,13 @@ function nekoPage(pageName) {
     .then((response) => {
       if (!response.ok) {
         trace("JSON error.", "warn");
-        error.play();
-        trace("Error play!", "note");
+        try {
+            trace("Error play!", "note");
+            error.play();
+        } catch (error) {
+            trace(`Can't play :(: ${error.message}`, "warn");
+        }
+        throw new Error("JSON fetch failed");
       }
       return response.json();
     })
@@ -96,47 +128,53 @@ function nekoPage(pageName) {
         banamiPfp.remove();
         banamiPfpStatus = false;
         trace("Removed avatar from state", "note");
-      } else {
-        trace("Avatar isn't on state. Doing nothing.", "note");
+      // commenting these lines out to prevent console spam (yucky)
+      // } else {
+        // trace("Avatar isn't on state. Doing nothing.", "note");
       }
-    
-    if (data.banamiBackground !== undefined) {
-      body.style.backgroundImage = `url(${data.banamiBackground})`;
-      body.style.position = "fixed";
-      body.style.top = "0";
-      body.style.left = "0";
-      body.style.width = "100%";
-      body.style.height = "100%";
-      body.style.backgroundRepeat = "no-repeat";
-      body.style.backgroundSize = "cover";
-      body.style.backgroundPosition = "center";
-      body.style.zIndex = "-1";
-    } else {
-      body.style.backgroundImage = '';
-      body.style.position = '';
-      body.style.top = '';
-      body.style.left = '';
-      body.style.width = '';
-      body.style.height = '';
-      body.style.backgroundRepeat = '';
-      body.style.backgroundSize = '';
-      body.style.backgroundPosition = '';
-      body.style.zIndex = '';
+
+      if (data.banamiBackground !== undefined) {
+        html.style.backgroundImage = `url(${data.banamiBackground})`;
+        html.style.position = "fixed";
+        html.style.top = "0";
+        html.style.left = "0";
+        html.style.width = "100%";
+        html.style.height = "100%";
+        html.style.backgroundRepeat = "no-repeat";
+        html.style.backgroundSize = "cover";
+        html.style.backgroundPosition = "center";
+        html.style.zIndex = "-1";
+      } else {
+        html.style.backgroundImage = '';
+        html.style.position = '';
+        html.style.top = '';
+        html.style.left = '';
+        html.style.width = '';
+        html.style.height = '';
+        html.style.backgroundRepeat = '';
+        html.style.backgroundSize = '';
+        html.style.backgroundPosition = '';
+        html.style.zIndex = '';
     }
-
-
-      
-      trace("Enter play!", "note");
-      enter.play();
+    
+    try {
+        trace("Enter play!", "note");
+        enter.play();
+    } catch (error) {
+        trace(`Can't play :(: ${error.message}`, "warn");
+    }
 
       backButton.style.display = "inline-block";
 
       currentPage = pageName;
     })
     .catch((error) => {
-      trace(`An error occurred: ${error.message}`, "warn");
-      error.play();
-      trace("Error play!", "note");
+        try {
+            trace("Error play!", "note");
+            error.play();
+        } catch (error) {
+            trace(`Can't play :(: ${error.message}`, "warn");
+        }
       titleMain.textContent = "Whoops!";
       textMain.innerHTML = "Don’t fret — let's give it another try! Please refresh the page, and if the problem presists, file a report at <a href='https://github.com/PukaCyi/Banami/issues'>Github</a>.";
     });
